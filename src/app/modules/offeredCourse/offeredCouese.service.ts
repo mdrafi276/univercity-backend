0ed6adbcfee03e23e6fc8db8487b7533cbaab076
mdrafi276 <mdrafi37473@gmail.com>
2024-06-07 12:42:40 +0600

    time management validation complete

--- src/app/modules/offeredCourse/offeredCouese.service.ts
+++ src/app/modules/offeredCourse/offeredCouese.service.ts
@@ -5,9 +5,10 @@ import { TOfferedCourse } from "./offeredCourse.interface"
 import { OfferCouese } from "./offeredCourse.modle"
 import { Course } from "../course/course.model";
 import { Faculty } from "../faculty/faculty.models";
+import { AcademicDepartment } from "../academicDepartment/academicDepartment.model";
 
 const createOfferedCoueseIntoDB = async (payload: TOfferedCourse) => {
-    const { semesterRegistration, academicFaculty, academicDepartment, course, faculty } = payload;
+    const { semesterRegistration, academicFaculty, academicDepartment, days, startTime, endTime, section, course, faculty } = payload;
     //check if the semester registration id is exist
 
     const isSemesterRegistrationExists = await SemesterRegistation.findById(semesterRegistration);
@@ -16,13 +17,13 @@ const createOfferedCoueseIntoDB = async (payload: TOfferedCourse) => {
     }
     const academicSemester = isSemesterRegistrationExists.academicSemester;
 
-    const isSemesterFacultyExists = await academicFaculty.findById(academicFaculty);
-    if (!isSemesterFacultyExists) {
+    const isAcademicFacultyExists = await academicFaculty.findById(academicFaculty);
+    if (!isAcademicFacultyExists) {
         throw new AppError(httpStatus.NOT_FOUND, 'Academic faculty not found !')
     }
 
-    const isSemesterDepartmnetExists = await academicDepartment.findById(academicDepartment);
-    if (!isSemesterDepartmnetExists) {
+    const isAcademicDepartmnetExists = await academicDepartment.findById(academicDepartment);
+    if (!isAcademicDepartmnetExists) {
         throw new AppError(httpStatus.NOT_FOUND, 'Academic departmnet not found !')
     }
 
@@ -35,7 +36,54 @@ const createOfferedCoueseIntoDB = async (payload: TOfferedCourse) => {
     if (!isFacultyExists) {
         throw new AppError(httpStatus.NOT_FOUND, 'Faculty not found !')
     }
-    const result = await OfferCouese.create(...payload, academicSemes)
+
+    // Check if the department is belong to the faculty 
+    const isDepartmentBelongToFaculty = await AcademicDepartment.findOne({
+        academicFaculty,
+        _id: academicDepartment,
+    });
+    if (!isDepartmentBelongToFaculty) {
+        throw new AppError(httpStatus.BAD_REQUEST, `This ${isAcademicDepartmnetExists.name} not belong to this ${isAcademicFacultyExists.name}`)
+    }
+
+    // check if the same offer course some section in same registered semester exists
+
+    const isSameOfferedCorseExistWithSameRegisteredSemesterWithSection = await OfferCouese.findOne({
+        semesterRegistration,
+        course,
+        section
+    })
+    if (isSameOfferedCorseExistWithSameRegisteredSemesterWithSection) {
+        throw new AppError(httpStatus.BAD_REQUEST, 'Offered course with same section is already exist!')
+    }
+
+    //get the schedule of the faculties
+
+    const assignedSchedules = await OfferCouese.find({
+        semesterRegistration,
+        faculty,
+        days: { $in: { days } },
+    }).select('days startTime endTime');
+
+    const newSchedule = {
+        days,
+        startTime,
+        endTime
+    }
+
+    assignedSchedules.forEach((schedule) => {
+        const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`)
+        const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`)
+        const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`)
+        const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`)
+
+        if (newStartTime < existingEndTime && newEndTime > existingStartTime) {
+            throw new AppError(httpStatus.CONFLICT, `This faculty is not availabe at that time ! Choose other time or Day`)
+
+        }
+    })
+    // return null;
+    const result = await OfferCouese.create(...payload, academicSemester)
     return result;
 
 }
--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -11,8 +11,21 @@ const createOfferedCourseValidationSchema = z.object({
         section: z.number(),
         maxCapacity: z.number(),
         days: z.enum([...Days] as [string, ...string[]]).optional(),
-        starttime: z.string(),
+        starttime: z.string().refine((time) => {
+            const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+            return regex.test(time)
+        }, {
+            message: 'Invalid time formate , expected "HH:MM" in 24 hourse formate'
+        }),
         endTime: z.string(),
+    }).refine((body) => {
+        //startTime: 10:30 =>1970-01-01T10:30
+        //endTime : 12:30 =>1970-01-01T12:30
+        const start = new Date(`1970-01-T${body.starttime}:00`)
+        const end = new Date(`1970-01-T${body.endTime}:00`)
+        return end > start
+    }, {
+        message: 'Start time should be before End time',
     })
 })
 
